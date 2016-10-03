bf67a64bc885dc9bfa0cc55b695a3272c73c7d8b
Toshiya Nakakura <nakakura@me.com>
2016-10-03 18:03:03 +0900

    ui

---
+++ Assets/JanusController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class JanusController{
+	private SocketIOComponent socket_;
+
+	public JanusController(){
+
+		//webrtc-server.paas.jp-e1.cloudn-service.com
+		//this.socket_.Connect ();
+		GameObject socket = GameObject.Find ("SocketIO");
+		this.socket_ = socket.GetComponent<SocketIOComponent> ();
+		this.socket_.On ("open", OnOpen);
+	}
+
+	public void OnOpen(SocketIOEvent e)
+	{
+		Debug.Log("=====");
+		Debug.Log ("[SocketIO] Open received: " + e.name + " " + e.data);
+	}
+
+	public void bind(string ipAddress){
+		Debug.Log("ipaddress");
+	}
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+using SocketIO;
+
+public class UIController: MonoBehaviour
+{
+	private JanusController controller_;
+	// Use this for initialization
+	void Start ()
+	{
+		this.controller_ = new JanusController();
+	}
+
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//print ("hogehoge");
+	}
+
+	public void OnClickBind() {
+		Debug.Log ("bind click!");
+		GameObject go = GameObject.Find ("JanusIpField");
+		InputField field = go.GetComponent<InputField> ();
+		Debug.Log (field.text);
+		this.controller_.bind(field.text);
+	}
+
+	public void OnClickLogin() {
+		Debug.Log ("login click!");
+		GameObject go = GameObject.Find ("PeerIdField");
+		InputField field = go.GetComponent<InputField> ();
+		Debug.Log (field.text);
+	}
+
+	public void OnClickCreateStream() {
+		Debug.Log ("stream click!");
+		GameObject a_go = GameObject.Find ("AudioPortField");
+		InputField a_field = a_go.GetComponent<InputField> ();
+		Debug.Log (a_field.text);
+
+		GameObject v_go = GameObject.Find ("VideoPortField");
+		InputField v_field = v_go.GetComponent<InputField> ();
+		Debug.Log (v_field.text);
+	}
+
+	public void OnClickCall() {
+		Debug.Log ("call click!");
+		GameObject go = GameObject.Find ("TargetPeerIdField");
+		InputField field = go.GetComponent<InputField> ();
+		Debug.Log (field.text);
+	}
+}
